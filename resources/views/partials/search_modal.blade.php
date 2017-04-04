4c70e01fc000037a76f5247cdf75680ce884b89a
rajesh surapuraju <rajeshs142@gmail.com>
2017-04-04 21:40:27 +0530

    new changes

---
+++ resources/views/partials/search_modal.blade.php
@@ -0,0 +1,10 @@
+<div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModalLabel">
+    <div class="modal-dialog" role="document">
+        <form role="search" method="GET" action="/search" class="text-center">
+            <div class="form-group row">
+                <input id="search-inpt-nav-top" name="search" type="text" class="form-control-lg form-control col-8" placeholder="Turkish Breads...">
+                <button id="search-btn-nav-top" type="submit" class="btn btn-lg btn-primary srch-btn col-4">Search</button>
+            </div>
+        </form>
+    </div>
+</div>
