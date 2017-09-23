badd70f1973cda35d0fde72d501cc5cc73bae9b0
sree vaibhav <vaibhavsree95@gmail.com>
2017-09-23 18:06:20 +0530

    font-size and price string splitting is fixed

--- resources/views/catalogue/catalogue1.blade.php
+++ resources/views/catalogue/catalogue1.blade.php
@@ -13,20 +13,28 @@
     				<div class="row">
     					<div class="col-7 px-2">
     						<div>
-    							<span class="align-top fz-1-8rem">$</span>
-    							<h2 class="d-inline-block fz-3rem">{{ $details->price }}</h2>
-    							<span class="align-topi d-inline-block">
-    								<div class="fz-1-4rem">30</div>
-    								<div class="fz-0-7rem">each</div>
-    							</span>
+                                <?php
+                                    $new_array = array();
+                                    if($details->price) {
+                                    $new_array = explode('.',$details->price);
+                                    }
+                                ?>
+                                @if (is_array($new_array) && count($new_array) > 0)
+                                    <span class="align-top fz-1-8rem">$</span>
+                                    <h2 class="d-inline-block fz-3rem">{{ $new_array[0] }}</h2>
+                                    <span class="align-topi d-inline-block">
+                                        <div class="fz-1-4rem">{{$new_array[1]}}</div>
+                                        <div class="fz-0-7rem">each</div>
+                                    </span>
+                                @endif
     						</div>
                             @if($details->action_btn)
         						<div class="bg-yellow py-2">
-        							<b>{{ $details->action_btn }}<span class="align-top fz-0-7rem">c</span></b>
+                                    <b>{{ $details->action_btn }}</b>
         						</div>
                             @endif
     						<div class="text-left fz-0-7rem p-2">
-    							<b>{{ $details->title }}</b>
+                                <b class="fz-14">{{ $details->title }}</b>
     							<div>{{ $details->weight }}</div>
     							<div>{{ $details->packsize }}</div>
     				        </div>
